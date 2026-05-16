a4fe0b78e47e4246a9bfb29377f2e15c0e4d5a6a
luismtapiab <lmtapia1024@gmail.com>
2026-05-15 22:23:06 -0400

    improving link to share

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,8 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Luis Tapia';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Luis Tapia - Software Engineer, researcher and musician. ';
+export const SITE_KEYWORDS = ['Software Engineer', 'Luis Tapia', 'Luis Tapia Bio', 'Blog', 'compilers', 'functional programming'];
+export const SOCIAL_LINKS = {
+    github: 'https://github.com/luismtapiab',
+    linkedin: 'https://linkedin.com/in/luismtapiab',
+};

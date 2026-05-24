dd405c6cbbb3bf05bd34d96ef8fe7a62a98499eb
luismtapiab <lmtapia1024@gmail.com>
2026-05-24 17:54:12 -0400

    little update to intro

--- src/data/i18n.ts
+++ src/data/i18n.ts
@@ -11,7 +11,7 @@ export const ui = {
     },
     home: {
       greeting: 'Luis Tapia',
-      intro: 'I\'m a developer and musician from Bolivia, this page contains some of the projects I did. I\'m interested in solving problems with the tools I know, more about the logic and taking apart the pieces rather the languages or software but I enjoy both. I\'m a developer but in my personal life I\'m also a caretaker, proud guardian of two dogs. I also like to travel and explore new places.',
+      intro: 'I\'m a developer and musician from Bolivia, this page contains some of the projects I did. I\'m interested in solving problems with the tools I know, more about the logic and taking apart the pieces rather the languages or software but I enjoy both. I\'m into compilers, medical informatics, and AI. I also like to travel and explore new places.',
       projects: 'Projects',
       compilers: 'Compilers & PL Research',
       other: 'Other Projects',
@@ -46,7 +46,7 @@ export const ui = {
     },
     home: {
       greeting: 'Luis Tapia',
-      intro: 'Soy desarrollador y músico de Bolivia, esta página contiene algunos de los proyectos que he realizado. Me interesa resolver problemas con las herramientas que conozco, más sobre la lógica y desarmar las piezas que los lenguajes o el software en sí, pero disfruto de ambos. Soy desarrollador, pero en mi vida personal también soy cuidador, orgulloso guardián de dos perros. También me gusta viajar y explorar nuevos lugares.',
+      intro: 'Soy desarrollador y músico de Bolivia, esta página contiene algunos de los proyectos que he realizado. Me interesa resolver problemas con las herramientas que conozco, más sobre la lógica y desarmar las piezas que los lenguajes o el software en sí, pero disfruto de ambos. Me interesa los compiladores, la informática médica y la IA. También me gusta viajar y explorar nuevos lugares.',
       projects: 'Proyectos',
       compilers: 'Compiladores e Investigación en LP',
       other: 'Otros Proyectos',
